0b488d82b395b78054c2a8b769115d65982c8b33
Eshana Sangeeth <106311525+EshSang@users.noreply.github.com>
2024-10-31 00:21:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/sidebar'
+
+const App = () => {
+  return (
+    <div className='dashboard'>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
