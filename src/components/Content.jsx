9449bad11d700c392917668db21c00cc2a10888b
Eshana Sangeeth <106311525+EshSang@users.noreply.github.com>
2024-11-04 23:24:55 +0530

    create content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import ContentHeader from "./ContentHeader";
 
 const Content = () => {
-  return (
-    <div>Content</div>
-  )
-}
+  return <div className="content">
+    <ContentHeader />
+  </div>;
+};
 
-export default Content
+export default Content;
