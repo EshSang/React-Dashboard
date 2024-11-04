dfae9ad49d38438200dcb08644fea21bd1cdb73f
Eshana Sangeeth <106311525+EshSang@users.noreply.github.com>
2024-11-04 23:44:48 +0530

    add content header

--- src/components/ContentHeader.jsx
+++ src/components/ContentHeader.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BiSearch } from "react-icons/bi";
 
 const ContentHeader = () => {
   return (
-    <div>ContentHeader</div>
-  )
-}
+    <div className="content--header">
+      <h1 className="header--title">Dashboard</h1>
+      <div className="header--activity">
+        <div className="search--box">
+          <input type="text" placeholder="Seach anything here...." />
+          <BiSearch />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ContentHeader
+export default ContentHeader;
